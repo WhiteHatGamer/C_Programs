26e62d5ba96b69881554c9ceb500dbbf1a879e19
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-09 20:33:26 +0530

    displayList

---
+++ dataStructure/src/linkedList.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkedList.h"
+
+//TODO: Display List
+void PrintList(linkedlist_t* _head){
+    linkedlist_t *tmp = _head;
+
+    while(tmp != NULL){
+        printf("%d -> ", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+//TODO: Create New Node
+/*linkedlist_t* createNode(linkedlist_t** _head, int _data){
+    linkedlist_t result;
+    result.data = _data;
+    result.next = NULL;
+    _head = &result;
+    return &result;
+}*/
+
+//TODO: Insert at head
+//TODO: Find Node
+//TODO: Update Node
