57350b3633168af7bfe20bb167661ae03b075f53
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-09 20:41:04 +0530

    nodeCreationFunction

--- dataStructure/src/linkedList.c
+++ dataStructure/src/linkedList.c
@@ -13,13 +13,15 @@ void PrintList(linkedlist_t* _head){
     printf("\n");
 }
 //TODO: Create New Node
-/*linkedlist_t* createNode(linkedlist_t** _head, int _data){
-    linkedlist_t result;
-    result.data = _data;
-    result.next = NULL;
-    _head = &result;
-    return &result;
-}*/
+linkedlist_t* createNode(linkedlist_t** _head, int _data){
+    linkedlist_t* result = (linkedlist_t*)malloc(sizeof(linkedlist_t));
+    if (result != NULL){
+        result->data = _data;
+        result->next = NULL;
+        *_head = result;
+    }
+    return result;
+}
 
 //TODO: Insert at head
 //TODO: Find Node
