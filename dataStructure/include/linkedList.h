cbf6db25f702d1cfad5d951cc5a1dd8e7d0ae89e
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-09 20:33:10 +0530

    linkedListHeader

---
+++ dataStructure/include/linkedList.h
@@ -0,0 +1,21 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct LinkedList{
+    int data;
+    struct LinkedList* next;
+}linkedlist_t;
+
+//TODO: Display List
+void PrintList(linkedlist_t* _head);
+
+//TODO: Create New Node
+linkedlist_t* createNode(linkedlist_t** _head, int _data);
+
+//TODO: Insert at head
+
+//TODO: Find Node
+
+//TODO: Update Node
+
+#endif
