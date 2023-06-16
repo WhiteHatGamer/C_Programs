a123b0882e859fd2111cce023ae613aeda0aac3c
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-16 17:10:03 +0530

    demoImplementation

--- dataStructure/src/main.c
+++ dataStructure/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "linkedList.h"
 
 int main(){
@@ -9,7 +10,7 @@ int main(){
     tmp = createNode(999);
     tmp->next = Head;
     Head = tmp;
-    for (int i = 0; i < 25; i++)
+    for (int i = 0; i < 15; i++)
     {
         tmp = createNode(i);
         tmp->next = Head;
@@ -19,17 +20,19 @@ int main(){
     PrintList(Head);
 
     // Inserting in the beginning/somewhere
-    linkedlist_t l4;
-    l4.data = 4;
-    l4.next = Head;
-    Head = &l4;
+    linkedlist_t* l4 = (linkedlist_t*)malloc(sizeof(linkedlist_t));
+    l4->data = 4;
+    l4->next = Head;
+    Head = l4;
 
     PrintList(Head);
 
     // Deleting a node from beggining
-    Head = Head->next;
+    deleteHead(&Head);
     
     PrintList(Head);
+
+    freeLinkedList(Head);
 
     return 0;
 }
