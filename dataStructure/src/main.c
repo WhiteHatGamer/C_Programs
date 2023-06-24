40c0c66775567c31d27486c9ee507a9bb030700a
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-24 13:58:47 +0530

    insertEndDeleteNodeinsertIndexImplemented

--- dataStructure/src/main.c
+++ dataStructure/src/main.c
@@ -4,15 +4,16 @@
 
 int main(){
     // Create New Node
-    linkedlist_t* tmp = createNode(999);
-    linkedlist_t* Head = tmp;
+    linkedlist_t* Head = createNode(999);
+
+    // Insert New Node
     for (int i = 0; i < 15; i++)
     {
-        tmp = createNode(i);
-        tmp->next = Head;
-        Head = tmp;
+        Head = insertHead(Head, i);
     }
     
+    // Printing whole List
+    printf("\nInsertHead");
     PrintList(Head);
 
     // Inserting in the beginning/somewhere
@@ -20,13 +21,31 @@ int main(){
     l4->data = 4;
     l4->next = Head;
     Head = l4;
-
+    printf("\nInsertnorml");
     PrintList(Head);
 
     // Deleting a node from beggining
     deleteHead(&Head);
-    
+    printf("\ndeleteHead");
     PrintList(Head);
+
+    for (int i=15; i<25; i++){
+        insertEnd(Head, i);
+    }
+    printf("\nInsertend");
+    PrintList(Head);
+
+    printf("Node 999 found: %i\n",findNode(Head, 999));
+    deleteNode(Head, 999);
+    printf("Node 999 found: %i\n",findNode(Head, 999));
+
+    insertIndex(Head, 9999, 5);
+    printf("\nInserindex");
+    PrintList(Head);
+//
+    //updateNode(Head, 25, 250);
+    //printf("\nInsertHead");
+    //PrintList(Head);
 
     freeLinkedList(Head);
 
