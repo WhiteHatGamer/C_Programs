600876033d41fc6592fcd6b825561f521c749ac8
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-24 20:06:47 +0530

    BinaryTreeSrcFile

---
+++ dataStructure/src/binaryTree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binaryTree.h"
+
+binarytree_t* createTree(int _data){
+    return;
+}
+
+void printTree(binarytree_t* _root){
+    return;
+}
+
+void freeTree(binarytree_t* _root){
+    free(_root);
+    return;
+}
