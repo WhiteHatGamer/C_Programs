59f1da6c3052ce34fe26e7410a798ee33f9c5618
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-24 20:09:55 +0530

    createTreeFunctionDefined

--- dataStructure/src/binaryTree.c
+++ dataStructure/src/binaryTree.c
@@ -3,7 +3,11 @@
 #include "binaryTree.h"
 
 binarytree_t* createTree(int _data){
-    return;
+    binarytree_t* branch = (binarytree_t*)malloc(sizeof(binarytree_t));
+    branch->data = _data;
+    branch->left = NULL;
+    branch->right = NULL;
+    return branch;
 }
 
 void printTree(binarytree_t* _root){
