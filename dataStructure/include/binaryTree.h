bb70be1490df4407a73e440a04e36c446c642067
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-06-26 17:04:35 +0530

    operationsFinalized

--- dataStructure/include/binaryTree.h
+++ dataStructure/include/binaryTree.h
@@ -7,9 +7,24 @@ typedef struct BinaryTree{
     struct BinaryTree* right;
 }binarytree_t;
 
-binarytree_t* createTree(int _data);
+binarytree_t* btCreateNode(int _data);
 
+// Insert a Node in Tree
+void btInsertNode(binarytree_t* _root, binarytree_t* _node);
+
+//TODO: search for a node in tree
+//TODO: bfs
+//TODO: dfs
+
+//TODO: Delete a node in tree
+//TODO: without child, one chile, two child
+
+//TODO: in order traverssal in a tree
 void printTree(binarytree_t* _root);
+
+//TODO: pre order traversal in a tree
+
+//TODO: post order traversal in a tree
 
 void freeTree(binarytree_t* _root);
 
