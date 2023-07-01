d52eac57bac83cd7a224e5c4408eb45d38d24472
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-07-01 13:19:00 +0530

    hashFunctionDefined

---
+++ dataStructure/src/hashTable.c
@@ -0,0 +1,16 @@
+#include "hashTable.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+unsigned int hash(char* _name){
+    int length = strnlen(_name, MAX_SIZE);
+    unsigned int hash_value = 0;
+    for (int i = 0; i < length; i++)
+    {
+        hash_value += _name[i];
+        hash_value = (hash_value*_name[i]) % TABLE_SIZE;
+    }
+    return hash_value;
+}
