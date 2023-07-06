4ecf212888f6bf20013a0a07e9e9cd905480e432
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-07-06 14:01:19 +0530

    htInsertFunctionDefined_Optimizations

--- dataStructure/src/hashTable.c
+++ dataStructure/src/hashTable.c
@@ -1,10 +1,16 @@
 #include "hashTable.h"
+#include "person.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
+person_t* hashTableArray[TABLE_SIZE];
+
 unsigned int hash(char* _name){
+    // if (_name==NULL){
+    //     return -1;
+    // }
     int length = strnlen(_name, MAX_SIZE);
     unsigned int hash_value = 0;
     for (int i = 0; i < length; i++)
@@ -22,10 +28,26 @@ void initHashTable(){
 }
 
 void printHashTable(){
-    for(int i=0;i<MAX_SIZE;i++){
+    printf("Start\n");
+    for(int i=0;i<TABLE_SIZE;i++){
         if(hashTableArray[i] == NULL){
-            printf("\t%i\t---", i);
+            printf("\t%i\t---\n", i);
         }
-        printf("\t%i\t---%s\n", i, hashTableArray[i]->name);
+        else{
+            printf("\t%i\t%s\n", i, hashTableArray[i]->name);
+        }
     }
+    printf("End\n");
+}
+
+bool htInsert(person_t* _person){
+    if (_person==NULL){
+        return false;
+    }
+    unsigned int index = hash(_person->name);
+    if(hashTableArray[index] != NULL){
+        return false;
+    }
+    hashTableArray[index] = _person;
+    return true;
 }
