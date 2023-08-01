3d2dcf2097a27a8db84bab317c25bea60b1092c0
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-08-01 17:57:13 +0530

    initTrieFunctionDefined

--- dataStructure/src/trie.c
+++ dataStructure/src/trie.c
@@ -3,3 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+trie_t* initTrie(){
+    trie_t* _root = (trie_t*)malloc(sizeof(trie_t));
+    if(!_root){
+        // NULL validation
+        return NULL;
+    }
+
+    for (int i=0;i<TRIE_SIZE;i++){
+        _root->child[i] = NULL;
+    }
+    _root->terminal = false;
+
+    return _root;
+}
