890b1d7282abbdb72ce7d7f76ce9990a1db2b1e9
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-08-01 18:05:28 +0530

    insertTrieFunctionDeclaredExtraComments

--- dataStructure/include/trie.h
+++ dataStructure/include/trie.h
@@ -5,16 +5,20 @@
 
 #define TRIE_SIZE 256
 
+// Struct Definition
 typedef struct Trie
 {
     struct Trie* child[TRIE_SIZE];
     bool terminal;
 }trie_t;
 
+// Trie Initialisation Returns -> trie_t*
 trie_t* initTrie();
 
+// Insert Into Trie
+bool insertTrie(trie_t** _root, char* _signedText);
+
+// Frees trie allocated by malloc
 bool freeTrie(trie_t* _root);
-
-
 
 #endif
