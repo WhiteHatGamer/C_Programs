567ef23ebf26064cd685a7fac48403814f6a5bab
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-08-04 19:01:06 +0530

    deleteTrieFunctionDeclared

--- dataStructure/include/trie.h
+++ dataStructure/include/trie.h
@@ -18,6 +18,9 @@ trie_t* initTrie();
 // Insert Into Trie
 bool insertTrie(trie_t** _root, char* _signedText);
 
+// Delete From Trie
+bool deleteTrie(trie_t* _root, char* _signedText);
+
 // Print Trie
 void printTrie(trie_t* _root);
 
