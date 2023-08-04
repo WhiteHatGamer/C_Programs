980724def889927e0532c89aaf519c4a00640889
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-08-04 19:01:21 +0530

    deleteTrieFunctionDefined

--- dataStructure/src/trie.c
+++ dataStructure/src/trie.c
@@ -42,6 +42,23 @@ bool insertTrie(trie_t** _root, char* _signedText){
     return true;
 }
 
+bool deleteTrie(trie_t* _root, char* _signedText){
+    if (_root == NULL){
+        return false;
+    }
+    unsigned char* text = (unsigned char*)_signedText;
+    trie_t* tmp = _root;
+    int length = strlen(_signedText);
+    for(int i=0;i<length;i++){
+        if(tmp->child[text[i]] == NULL){
+            return false;
+        }
+        tmp = tmp->child[text[i]];
+    }
+    tmp->terminal = false;
+    return true;
+}
+
 void printTrieRec(trie_t* _node, unsigned char* _prefix, int length){
     unsigned char _newPrefix[length+2];
     memcpy(_newPrefix, _prefix, length);
