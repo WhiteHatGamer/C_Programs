530cfef66ff6ae7f4871c83591dda457dfb0e741
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-08-15 14:57:52 +0530

    graphInitReDeclaredFreed

--- dataStructure/src/graph.c
+++ dataStructure/src/graph.c
@@ -1,7 +1,16 @@
 #include "graph.h"
 #include <stdio.h>
 
-graph_t* graphInit(void* _data){
+graph_t* graphInit(){
     graph_t* node = (graph_t*)malloc(sizeof(graph_t));
-    node->data = _data;
+    node->data = NULL;
+    node->edges = NULL;
+}
+
+bool insertGraph(graph_t* _graph, void* _data){
+    
+}
+
+bool freeGraph(graph_t* node){
+    free(node);
 }
