25ea11f4fe335e014aac4d379ac3f051d0e0ab35
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-09-02 15:09:34 +0530

    qInitFunctionDefined

--- dataStructure/src/queue.c
+++ dataStructure/src/queue.c
@@ -8,3 +8,14 @@ typedef struct Queue {
     linkedlist_t* front;
     linkedlist_t* rear;
 }queue_t;
+
+queue_t* qInit(){
+    queue_t* q = (queue_t*)malloc(sizeof(queue_t));
+    if (q == NULL){
+        // malloc failed 
+        return NULL;
+    }
+    q->front = q->rear = NULL;
+    return q;
+}
+
