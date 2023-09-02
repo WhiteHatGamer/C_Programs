307913c6eebb1ba83a005b30709a15337cffcf11
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-09-02 14:55:08 +0530

    llUpdateNodeFunctionDeclared

--- dataStructure/include/linkedList.h
+++ dataStructure/include/linkedList.h
@@ -29,6 +29,9 @@ void llInsertEnd(linkedlist_t* _head, void* _data);
 void llInsertIndex(linkedlist_t* _head, void* _data, int index);
 
 
+// Update Node -> void*
+bool llUpdateNode(linkedlist_t* _head, void* _data, void* _newData);
+
 // Merge Linked List -> linkedlist_t
 linkedlist_t* llMerge(linkedlist_t* _head1, linkedlist_t* _head2);
 
