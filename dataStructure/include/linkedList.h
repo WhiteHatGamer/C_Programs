8eb8e40dd1a562faef2abe1639507b71cf7c8a30
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-09-02 14:53:13 +0530

    functionNamesReDeclared

--- dataStructure/include/linkedList.h
+++ dataStructure/include/linkedList.h
@@ -8,20 +8,20 @@ typedef struct LinkedList{
 
 
 // Create New Node
-linkedlist_t* createNode(void* _data);
+linkedlist_t* llCreateNode(void* _data);
 
 
 // Insert at head
-linkedlist_t* insertHead(linkedlist_t* _head, void* _data);
+linkedlist_t* llInsertHead(linkedlist_t* _head, void* _data);
 
 // Insert at End -> linkedlist_t
-void insertEnd(linkedlist_t* _head, void* _data);
+void llInsertEnd(linkedlist_t* _head, void* _data);
 
 // Insert a Node at an Index startin at 0 -> linkedlist_t
-void insertIndex(linkedlist_t* _head, void* _data, int index);
+void llInsertIndex(linkedlist_t* _head, void* _data, int index);
 
 
 // Free Memory
-void freeLinkedList(linkedlist_t* _head);
+void llFree(linkedlist_t* _head);
 
 #endif
